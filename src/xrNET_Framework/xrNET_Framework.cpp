ce297f66e326d219658fc7d05f0fc8401c008dc4
i-love-kfc <stepadunaevskij@gmail.com>
2022-10-03 20:44:39 +0400

    подключил xrNetFramework
    Пока что в нём всего одна функция: проверка на наличие файла на сервере.

---
+++ src/xrNET_Framework/xrNET_Framework.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+
+#include "xrNET_Framework.h"
+
+using namespace System;
+using namespace System::Web;
+using namespace System::Net;
+
+bool HasWebFile(const char* web_path)
+{
+    String^ path = gcnew String(web_path);
+    auto webRequest = HttpWebRequest::Create(path);
+    webRequest->Method = "HEAD";
+    try
+    {
+        webRequest->GetResponse()->Close();
+        return true;
+    }
+    catch (...)
+    {
+        return false;
+    }
+}
---
+++ src/xrNET_Framework/xrNET_Framework.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#ifdef XRNETFRAMEWORK_EXPORTS
+#define XRNETFRAMEWORK_API _declspec(dllexport)
+#else
+#define XRNETFRAMEWORK_API _declspec(dllimport)
+#endif
+
+XRNETFRAMEWORK_API bool HasWebFile(const char* web_path);
